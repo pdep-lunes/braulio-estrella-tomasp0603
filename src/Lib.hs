faa0de6044c88c33d8a14770716a2df4c8391029
tomasp0603 <tommpe.03@gmail.com>
2025-04-28 11:27:44 -0300

    Se agrega datatype Personaje y funciones iniciales (no funcional)

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,3 +4,36 @@ import Text.Show.Functions ()
 
 doble :: Int -> Int
 doble x = x * 2
+
+data Personaje = unPersonaje {
+    nombre :: String,
+    poder :: String,
+    superPoder :: String,
+    superPoderActivo :: Bool,
+    vida :: Int
+}
+
+agregarEspinaNombre :: Personaje->Personaje
+agregarEspinaNombre unPersonaje = nombre unPersonaje + "Espina estuvo aqui"
+
+desactivarSuper :: Personaje->Personaje
+desactivarSuper unPersonaje 
+  | superPoderActivo unPersonaje = not.superPoderActivo unPersonaje
+  | otherwise = superPoderActivo unPersonaje
+
+bolaEspinosa :: Personaje->Personaje
+bolaEspinosa unPersonaje
+  | vida unPersonaje >= 1000 = vida unPersonaje - 1000
+  | otherwise = vida unPersonaje - vida unPersonaje
+
+lluviaDeTuercas :: Personaje->Bool->Personaje
+lluviaDeTuercas unPersonaje esSanadora 
+  | esSanadora = vida unPersonaje + 800
+  | otherwise = vida unPersonaje - div (vida unPersonaje) 2
+
+
+
+granadaDeEspinas :: Int->Personaje->Personaje
+granadaDeEspinas radioExplosion unPersonaje
+  | radioExplosion > 3 && unPersonaje vida < 800 = desactivarSuper unPersonaje vida unPersonaje - vida unPersonaje  
+  | radioExplosion > 3 = 
